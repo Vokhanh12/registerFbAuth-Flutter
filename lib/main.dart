ca7a82f782f8f4d1f41f1e00c4411c37b2be6bda
VoKhanh <khanh>
2023-07-11 14:48:00 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MaterialApp(
     title: 'Flutter Demo',
     theme: ThemeData(primarySwatch: Colors.blue),
@@ -43,47 +44,76 @@ class _homePageState extends State<homePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Hello world'),
+        title: const Text('Register'),
       ),
-      body: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              TextField(
-                controller: _email,
-                keyboardType: TextInputType.emailAddress,
-                enableSuggestions: false,
-                autocorrect: false,
-                decoration: InputDecoration(labelText: 'Email'),
-              ),
-              SizedBox(height: 16.0),
-              TextField(
-                controller: _passworld,
-                obscureText: true,
-                enableSuggestions: false,
-                autocorrect: false,
-                decoration: InputDecoration(labelText: 'Password'),
-              ),
-              SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: () async {
-                  await Firebase.initializeApp(
-                    options: DefaultFirebaseOptions.currentPlatform,
-                  );
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              return Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 20.0),
+                      child: Image.asset(
+                        './lib/assets/11242654.png',
+                        width: 150.0,
+                        height: 150.0,
+                      ),
+                    ),
+                    TextField(
+                      controller: _email,
+                      keyboardType: TextInputType.emailAddress,
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      decoration: InputDecoration(labelText: 'Email'),
+                      style: TextStyle(fontSize: 17.0),
+                    ),
+                    SizedBox(height: 16.0),
+                    TextField(
+                      controller: _passworld,
+                      obscureText: true,
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                      ),
+                      style: TextStyle(fontSize: 17.0),
+                    ),
+                    SizedBox(height: 16.0),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 15.0),
+                      child: ElevatedButton(
+                        onPressed: () async {
+                          final email = _email.text;
+                          final password = _passworld.text;
 
-                  // Xử lý khi nút được nhấn
-                  final email = _email.text;
-                  final password = _passworld.text;
+                          final UserCredential userCredential =
+                              await FirebaseAuth.instance
+                                  .createUserWithEmailAndPassword(
+                            email: email,
+                            password: password,
+                          );
 
-                  final UserCredential = FirebaseAuth.instance
-                      .createUserWithEmailAndPassword(
-                          email: email, password: password);
-                  print(UserCredential);
-                },
-                child: Text('Register'),
-              ),
-            ],
-          )),
+                          print(userCredential);
+                        },
+                        child: Text('Register'),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+
+            default:
+              return const Text('Loading....');
+          }
+        },
+      ),
     );
   }
 }
